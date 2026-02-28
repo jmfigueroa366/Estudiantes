0d71e4787d283941b9fa26331b17a8b5a46fe66d
aacadavid <aacadavid@unicesar.edu.co>
2026-02-28 18:26:29 -0500

    feat: agregar encapsulamiento a clase Asignatura

--- src/git_hud/Asignatura.java
+++ src/git_hud/Asignatura.java
@@ -10,8 +10,18 @@ package git_hud;
  */
 public class Asignatura {
     
-    String codigo;
-    String nombre;
-    int creditos;
-    String docente;
+    private String codigo;
+    private String nombre;
+    private int creditos;
+    private String docente;
+
+    public Asignatura() {
+    }
+
+    public Asignatura(String codigo, String nombre, int creditos, String docente) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+        this.docente = docente;
+    }
 }
