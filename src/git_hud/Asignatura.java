ee0abaa8d7b9b4e3695d7a0edfbbd6711ad4c44d
aacadavid <aacadavid@unicesar.edu.co>
2026-02-28 18:29:47 -0500

    feat: agregar toString a clase Asignatura junto con getter y setters

--- src/git_hud/Asignatura.java
+++ src/git_hud/Asignatura.java
@@ -24,4 +24,43 @@ public class Asignatura {
         this.creditos = creditos;
         this.docente = docente;
     }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public String getDocente() {
+        return docente;
+    }
+
+    public void setDocente(String docente) {
+        this.docente = docente;
+    }
+
+    @Override
+    public String toString() {
+        return "Asignatura{" + "codigo=" + codigo + ", nombre=" + nombre + ", creditos=" + creditos + ", docente=" + docente + '}';
+    }
+    
+    
 }
