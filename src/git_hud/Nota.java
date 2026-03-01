944391bf47bd40b6396162b45d81c48f6ec647d8
aacadavid <aacadavid@unicesar.edu.co>
2026-02-28 19:08:08 -0500

    feat: crear clase Nota con atributos

---
+++ src/git_hud/Nota.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package git_hud;
+
+/**
+ *
+ * @author alvar
+ */
+public class Nota {
+    // atributos temporales hasta que se cree la clase estudiante, luego se cambia//
+    String codigoEstudiante;
+    String codigoAsignatura;
+    double valor;
+    //
+    String codigo;
+    String nombre;
+    int creditos;
+    String docente;
+}
