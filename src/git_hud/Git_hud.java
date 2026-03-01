1553debd44f6f3ce46b46a032863d4a9b31f5803
HaroldFlorez7 <hsflorez@unicesar.edu.co>
2026-03-01 09:08:20 -0500

    Constructor+SetteryGetter

--- src/git_hud/Git_hud.java
+++ src/git_hud/Git_hud.java
@@ -14,7 +14,56 @@ public class Git_hud {
     private String apellido;
     private int edad; 
     private int semestre;
+
+    public Git_hud(String codigo, String nombre, String apellido, int edad, int semestre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.semestre = semestre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
     
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+    
+
     
     
 
