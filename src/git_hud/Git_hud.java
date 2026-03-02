517e605e1e699e54e73bee0126d85541b53540e9
jesus figueroa <jmfigueroa@unicesar.edu.co>
2026-03-02 08:45:54 -0500

    Recupera git_hud desde main en rama lider

---
+++ src/git_hud/Git_hud.java
@@ -0,0 +1,170 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_hud;
+
+/**
+ *
+ * @author jesus
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Git_hud {
+
+    private static ArrayList<Asignatura> asignatura = new ArrayList <> ();
+    private static ArrayList<Asignatura> asignaturaEstudiante = new ArrayList <> ();
+    private static Scanner sc = new Scanner (System.in);
+    
+        /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    private static void registrarAsignatura() {
+        boolean registrarOtro = true;
+        boolean isFind = true;
+        
+        Asignatura asignaturaEncontrada = new Asignatura ();
+        while(registrarOtro) {
+            try {
+                isFind=false;
+                boolean asignaturaNoRegistrada=true;
+                System.out.println("¿Desea registrar una asignatura? (S/N)");
+                String tipo = sc.nextLine().toUpperCase();
+                registrarOtro = tipo.equals("S");
+                if(registrarOtro) {
+                    System.out.println("Ingrese el codigo de la asignatura que desea registrar: ");
+                    String codigo = sc.nextLine();
+                    for (Asignatura as: asignatura) {
+                        if (codigo.equals(as.codigo)) {
+                            isFind=true;
+                            asignaturaEncontrada=as;
+                            break;
+                        }
+                    }
+                    if(!isFind) {
+                        System.out.println("La materia no existe");
+                    } else {
+                        System.out.println("Materia encontarda, codigo:" + asignaturaEncontrada.codigo + ", nombre: " + asignaturaEncontrada.nombre);
+                        System.out.println("¿Esta seguro que desea matricular esta asignatura? (S/N)");
+                            String conf = sc.nextLine().toUpperCase();
+                            boolean confirmar = conf.equals("S");
+                            if (confirmar) {
+                                for (Asignatura as: asignaturaEstudiante) {
+                                    if (as.codigo.equals(asignaturaEncontrada.codigo)) {
+                                        asignaturaNoRegistrada=false;
+                                        System.out.println("Asignatura ya matruculada!!!");
+                                        break;
+                                    }
+                                }
+                                if (asignaturaNoRegistrada) {
+                                    asignaturaEstudiante.add(asignaturaEncontrada);
+                                    System.out.println("Asignatura registrada con exito!!!");
+                                }
+                            }
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+    
+    private static void listarAsignatura() {
+        if (asignaturaEstudiante.isEmpty()) {
+            System.out.println("No hay asignaturas registradas");
+        } else {
+            System.out.println("Asinatura registrada por el estudiante: ");
+            for (Asignatura e: asignaturaEstudiante) {
+                System.out.println("Codigo: " + e.codigo + ", Nombre: " + e.nombre + ", Creditos: " + e.creditos + ", Docente: " + e.docente);
+            }
+        }
+    }
+
+    private static void buscarAsignatura () {
+        boolean encontrado=false;
+        System.out.println("Ingrese el codigo de la asignatura que desea buscar:");
+        String codigo = sc.nextLine();
+        for (Asignatura e: asignaturaEstudiante) {
+            if (e.codigo.equalsIgnore(codigo)) {
+                encontrado=true;
+                System.out.println("Materia encontrada: Nombre: " + e.nombre + ", Creditos: " + e.creditos + ", Docente: " + e.docente);
+                break;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("Materia no encontrada");
+        }
+    }
+    
+    private Asignatura buscarAsignaturaPorCodigo (String codigo) {
+        for (Asignatura e: asignaturaEstudiante) {
+            if (e.codigo.equalsIgnoreCase(codigo)) {
+                return e;
+            }
+        }
+        return null;
+    }
+    
+    private static void actualizarAsignatura() {
+        System.out.println("Ingrese el codigo de la asignatura a actualizar: ");
+        String codigo = sc.nextLine();
+        
+        Asignatura encontrada = buscarAsignaturaPorCodigo (codigo);
+        if (encontrada == null) {
+            System.out.println("Asignatura no encontrada");
+        } else {
+            System.out.println("Asignatura actual: " + encontrada);
+            System.out.println("Ingrese nuevo nombre: ");
+            String nuevoNombre = sc.nextLine();
+            encontrada.nombre=nuevoNombre;
+            
+            System.out.println("Ingrese actualización de la cantidad de creditos: ");
+            int nuevosCreditos = Integer.parseInt(sc.nextLine());
+            encontrada.creditos=nuevosCreditos;
+            
+            System.out.println("Ingrese nuevo nombre del docente: ");
+            String nuevoDocente = sc.nextLine();
+            encontrada.docente=nuevoDocente;
+            
+            System.out.println("Asignatura actualizada con exito: " + encontrada);
+        }
+    }
+    
+    private static void eliminarAsignatura() {
+        System.out.println("Ingrese el codigo de la asignatura que desea eliminar: ");
+        String codigo = sc.nextLine();
+        Asignatura encontrar = buscarAsignaturaPorCodigo (codigo);
+        if (encontrar == null) {
+            System.out.println("Asignatura no encontrada");
+        } else {
+            System.out.println("Informacion de la asignatura: Nombre: " + encontrar.nombre + ", creditos: " + encontrar.creditos + ", docente: " + encontrar.docente);
+            System.out.println("¿Esta seguro que desea eliminar esta asignatura? (S/N)");
+            String conf = sc.nextLine().toUpperCase();
+            boolean confirmar = conf.equals("S");
+            if (confirmar) {
+                boolean eliminada=false;
+                for (Asignatura as: asignaturaEstudiante) {
+                    if (as.codigo.equals ( encontrar.codigo)) {
+                        asignaturaEstudiante.remove(as);
+                        System.out.println("Asignatura eliminada con exito!");
+                        eliminada=true;
+                        break;
+                    }
+                }
+                if (!eliminada) {
+                    System.out.println("La asignatura no esta registrada");
+                }
+            } else {
+                System.out.println("Cancelado...");
+            }
+        }
+    }
+}
+
+
